baefbb7b8f9062d03c0a03dd2003707d5018446c
wakaru <wakaru44@gmail.com>
2016-05-26 18:41:29 +0200

    better lambda handler

--- mylambda.py
+++ mylambda.py
@@ -1,13 +1,25 @@
+#!/usr/bin/python
+# -*- utf-8 -*-
+
 
 import datetime
+import logging
 import json
 
-def my_handler(event, context):
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
     """
     minimal AWS lambda handler
     """
+    logger.info("Yay!")
     msg={}
-    msg["event"]=event
-    msg["context"]=context
+    msg["event"]=repr(event)
+    msg["context"]=repr(context)
     msg["timestamp"]=datetime.datetime.now().strftime("%F - %H:%M")
     return { "message":json.dumps(msg) }
+
+if __name__=="__main__":
+    print lambda_handler("evento","contesto")
